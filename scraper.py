702d66445f85d6f3519e3baf881b1a5cfd5e38c5
Filip Matic <filipmatic.design@gmail.com>
2026-01-14 10:22:05 +0100

    Added league scraping and team url extraction

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ from playwright.sync_api import sync_playwright
 import time
 from bs4 import BeautifulSoup, Comment
 import json
+import re
 
 class theScraper:
     def __init__(self):
@@ -50,6 +51,33 @@ class theScraper:
 
             return html
         
+    def extract_ids(self, soup, table_id):
+        team_ids={}
+
+        comments=soup.find_all(string=lambda text: isinstance(text, Comment))
+
+        for comment in comments:
+            if f'id="{table_id}"' in comment:
+                comment_soup=BeautifulSoup(comment, "html.parser")
+                table=comment_soup.find("table",{"id":table_id})
+
+                if table:
+                    for row in table.find_all("tr"):
+                        squad_cell = row.find('th', {'data-stat': 'team'}) or row.find('td', {'data-stat': 'team'})
+
+                        if squad_cell:
+                            link=squad_cell.find("a")
+                            if link and link.get("href"):
+                                team_name=link.get_text(strip=True)
+                                href=link.get("href")
+
+                                match=re.search(r"/en/squads/([a-f0-9]{8})/", href)
+                                if match and team_name:
+                                    team_id=match.group(1)
+                                    team_ids[team_name]=team_id
+                        break
+            return team_ids
+                        
     def check_comments(self, soup, table_id):
         comments=soup.find_all(string=lambda text: isinstance(text, Comment))
 
@@ -79,13 +107,38 @@ class theScraper:
         except Exception as e:
             print(f"Error parsing tables: {e}")
 
-        return None
+        return None 
+
+    def scrape_league(self, html):
+        soup=BeautifulSoup(html, "html.parser")
+        table=None
+        team_ids={}
+        table_id = "results2024-202591_overall"
+
+        team_ids = self.extract_ids(soup, table_id)
+        table=self.check_comments(soup,"results2024-202591_overall")
+
+        if table is None:
+            required_columns=["Rk","Squad","Attendance","Top Team Scorer","Goalkeeper"]
+            table=self.check_columns(html, required_columns)
+        
+        if table is not None:
+            table=self._clean_match_logs(table)
+            if 'Squad' in table.columns:
+                table['team_id'] = table['Squad'].map(team_ids)
+                print(f"✓ Added team_id column to DataFrame")
+            
+            return table, team_ids
+        else:
+            raise Exception("Could not find match logs table on page.")
+        
+
     
     def scrape_matches(self,html, season="2024-2025"):
         soup=BeautifulSoup(html, "html.parser")
         match_logs_df=None
         
-        match_logs_df=self.check_comments(soup,"match_logs_df")
+        match_logs_df=self.check_comments(soup,"matchlogs_for")
 
         if match_logs_df is None:
             required_columns=["Date", "Comp", "Round", "Venue", "Result"]
@@ -126,23 +179,42 @@ class theScraper:
             
         return df
     
+    def build_team_urls(self, team_ids, season="2024-2025"):
+        team_urls={}
+
+        for team_name, team_id in team_ids.items():
+            team_slug=team_name.replace(' ','-')
+            url=f"https://fbref.com/en/squads/{team_id}/{season}/{team_slug}-Stats"
+            team_urls[team_name]=url
+
+        return team_urls
+    
     def scrape_multiple(self, team_urls, season, delay=4):
         pass
 
 if __name__=="__main__":
     scraper=theScraper()
 
-    url=f"https://fbref.com/en/squads/8602292d/2024-2025/Aston-Villa-Stats#all_matchlogs"
-    html=scraper.scrape_setup(url)
+    url_prem=f"https://fbref.com/en/comps/9/2024-2025/2024-2025-Premier-League-Stats"
+    html_prem=scraper.scrape_setup(url_prem)
 
     try:
-        df_matches=scraper.scrape_matches(html)
-        df_players=scraper.scrape_players(html)
+        df_teams,team_ids=scraper.scrape_league(html_prem)
+        df_teams.to_csv("prem.csv",index=False)
 
-        print(f"Successfully scraped {len(df_matches)} matches")
-        print(f"Successfully scraped {len(df_players)} matches")
+        team_urls=scraper.build_team_urls(team_ids, "2024-2025")
+        print(team_urls)
 
-        df_matches.to_csv("villa.csv", index=False)
-        df_players.to_csv("villa_players.csv", index=False)
+        # df_matches=scraper.scrape_matches(html)
+        # df_players=scraper.scrape_players(html)
+
+        # print(f"Successfully scraped {len(df_matches)} matches")
+        # print(f"Successfully scraped {len(df_players)} matches")
+
+        # df_matches.to_csv("villa.csv", index=False)
+        # df_players.to_csv("villa_players.csv", index=False)
+
+        
+
     except Exception as e:
         print(f"Error {e}")
